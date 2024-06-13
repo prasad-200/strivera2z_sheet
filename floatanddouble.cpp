c7016f0ca5d7dca4392360c640003039f6d6f876
Prasad Patil <prasadpatil8718@gmail.com>
2024-06-13 22:56:52 +0530

    basic cpp programs

---
+++ floatanddouble.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+ int age;
+ cin>>age;
+ cout<<age;
+ if(age>57){
+  cout<<"retire hoja";
+ }
+else{
+  if(age>=55 && age<=57){
+    cout<<"retirement aagi hai";
+  }
+  else if(age>=18){
+    cout<<""
+  }
+}
+  return 0;
+}
+
+    
