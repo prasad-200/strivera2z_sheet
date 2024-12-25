5e25dc5900841ae0ba7c99be0db8f39e1591dc86
prasad-200 <134679065+prasad-200@users.noreply.github.com>
2024-12-25 23:17:45 +0530

    Create pattern-9.cpp

---
+++ pattern-9.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter the number of rows:"<<endl;
+    cin>>n;
+     for(int i=1;i<=n;i++){
+     for(int j=1;j<=(2*n-1);j++){
+         if(j<=n-i){
+             cout<<"  ";
+         }
+         if(j>n-i && j<=(2*n-1)-(n-i)){
+             cout<<"* ";
+         }
+        if(j>(2*n-1)-(n-i)){
+           cout<<"  ";
+     }
+    }
+    cout<<endl;
+    }
+
+    for(int i=1;i<=n;i++){
+     for(int j=1;j<=(2*n-1);j++){
+         if(j<=i-1){
+             cout<<"  ";
+         }
+         if(j>i-1 && j<=(2*n-1)-(i-1)){
+             cout<<"* ";
+         }
+        if(j>(2*n-1)-(i-1)){
+           cout<<"  ";
+     }
+    }
+    cout<<endl;
+    }
+    return 0;
+}
