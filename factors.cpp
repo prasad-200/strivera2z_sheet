7661300758e79c4b942da36555c37d9965f9a89d
prasad-200 <134679065+prasad-200@users.noreply.github.com>
2025-03-06 23:39:37 +0530

    Update factors.cpp

--- factors.cpp
+++ factors.cpp
@@ -22,6 +22,30 @@ int main() {
        }
        cout<<a[i]<<", ";
    }
+   //optimal 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int n,c=0;
+   cout<<"Enter the number: "<<endl;
+   cin>>n;
+  vector<int>v;
+  for(int i=1;i<=sqrt(n);i++){
+      if(n%i==0){
+         v.push_back(i);
+         if(n/i!=i){
+             v.push_back(n/i);
+         }
+      }
+ }
+ sort(v.begin(),v.end());
+ for(auto it:v){
+     cout<<(it)<<" ";
+ }
+  
+    return 0;
+}
    cout<<"]"<<endl;
     return 0;
 }
