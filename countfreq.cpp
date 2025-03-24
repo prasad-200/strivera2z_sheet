699519432072278c8e5eba73f2c26850a5411f8e
Prasad <prasadpatil8718@gmail.com>
2025-03-24 23:14:29 +0530

    countfreqhashing

---
+++ countfreq.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number of elements:"<<endl;
+    cin>>n;
+    cout<<"Enter the numbers:"<<endl;
+    int arr[n];
+    map<int,int>mp;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        mp[arr[i]]++;
+    }
+   cout<<"frequencies are:"<<endl;
+   for(auto it:mp){
+      cout<<it.first<<" -> "<<it.second<<endl;
+   }
+   return 0;
+}
